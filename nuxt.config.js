856634a591855748409b14890fd7529859f02e7f
Philo van Kemenade <philovankemenade@gmail.com>
2018-02-27 16:17:23 +0000

    💪 ADD ALL THE FAVICONS

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,12 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'manifest', href: 'site.webmanifest' },
+      { rel: 'apple-touch-icon', href: 'icon.png' },
+      { rel: 'icon', type: 'image/png', href: 'favicon-16x16.png', sizes: "16x16"},
+      { rel: 'icon', type: 'image/png', href: 'favicon-32x32.png', sizes: "32x32"},
+      { rel: 'icon', type: 'image/png', href: 'favicon-96x96.png', sizes: "96x96"},
+      { rel: 'icon', type: 'image/png', href: 'favicon-194x194.png', sizes: "194x194"},
     ],
     script: [      
       { src: 'js/modernizr.custom.js' },
