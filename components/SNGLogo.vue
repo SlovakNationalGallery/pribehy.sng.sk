e38fa87280c05455fe942bcf29c9ec443b253b2f
Philo van Kemenade <philovankemenade@gmail.com>
2018-03-05 18:04:21 +0100

    add SNG logo to header

---
+++ components/SNGLogo.vue
@@ -0,0 +1,9 @@
+<template>
+  <img src="/img/logo-sng-black.svg" alt="SNG" class="sng-logo">
+</template>
+
+<style scoped>
+  .sng-logo {
+    width: 24px;
+  }
+</style>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,19 +1,21 @@
 <template>
-  <header class="my-3">
-    <div class="container">
-      <nav class="text-right">
-        <LanguageSwitch/>
-      </nav>
-    </div>
+  <header class="m-4">
+    <nav class="d-flex justify-content-between">
+      <a :href="$t('TheHeader.sngURL')">
+        <SNGLogo/>
+      </a>
+      <LanguageSwitch/>
+    </nav>
   </header>
 </template>
 
 <script>
 import LanguageSwitch from '@/components/LanguageSwitch'
+import SNGLogo from '@/components/SNGLogo'
 
 export default {
   components: {
-    LanguageSwitch,
+    LanguageSwitch, SNGLogo,
   },
 }
 </script>
